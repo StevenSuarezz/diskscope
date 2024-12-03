a08189a496c2605329cc46ea2308f678fb188954
Steven Suarez <suarezsteven30@gmail.com>
2024-12-02 23:08:24 -0500

    Initial commit

---
+++ build-and-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+rm -rf build/WSL/*
+
+mkdir -p build/WSL
+
+echo "====================="
+echo "Running CMake..."
+echo "====================="
+
+cmake -S . -B ./build/WSL
+
+cd build/WSL/
+
+echo "====================="
+echo "Running MakeFile..."
+echo "====================="
+
+make
+
+echo "====================="
+echo "Running executable..."
+echo "====================="
+
+./bin/diskscope
